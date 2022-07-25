a75c1e4d9941f5bb00df934b9e802b73cae6dad9
perdiDev <perdidev@gmail.com>
2022-07-25 12:13:34 +0800

    Mengubah tipe data room meenjadi integer

---
+++ prisma/migrations/20220725040933_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "MataKuliah" ALTER COLUMN "room" SET DATA TYPE VARCHAR(255);
--- src/mata_kuliah/mata_kuliah.service.ts
+++ src/mata_kuliah/mata_kuliah.service.ts
@@ -34,7 +34,7 @@ export class MataKuliahService {
         const data = {
             code: dataInput.code,
             name: dataInput.name,
-            room: Number(dataInput.room),
+            room: dataInput.room,
             class: dataInput.class,
             prodi: dataInput.prodi
         }
@@ -53,7 +53,7 @@ export class MataKuliahService {
                 data: {
                     code: dataInput.code,
                     name: dataInput.name,
-                    room: Number(dataInput.room),
+                    room: dataInput.room,
                     class: dataInput.class,
                     prodi: dataInput.prodi
                 }
@@ -63,7 +63,7 @@ export class MataKuliahService {
                 where: { id: +id },
                 data: {
                     name: dataInput.name,
-                    room: Number(dataInput.room),
+                    room: dataInput.room,
                     class: dataInput.class,
                     prodi: dataInput.prodi
                 }
